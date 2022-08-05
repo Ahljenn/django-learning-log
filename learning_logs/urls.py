1818968facba9eddfdd6716b85da927cc2b63996
Ahljenn <ahljennm2@gmail.com>
2022-08-05 00:06:16 -0700

    Added topics routing

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,12 +1,19 @@
 """Defines URL patterns for the learning_logs app."""
 
 from django.urls import path
-from . import views #Imports the views.py module from the same directory as the current urls.py module.
+from . import views 
+#Imports the views.py module from the same directory as the current urls.py module.
 
 app_name = "learning_logs"
 urlpatterns = [
-  path('', views.index, name='index'), #Home page
-  #Arg 1. URL - Base URL here.
+  #Arg 1. URL / Path
   #Arg 2. Specifies which function to call in views.py.
   #Arg 3. Provides the name for this URL pattern to be referred to in other code sections.
+  
+  # Home page
+  path('', views.index, name='index'), #Home page
+
+  # Page for all topics
+  path('/topics', views.topics, name='topics'),
+  
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
   """Home page for Learning Log app."""
   return render(request, 'learning_logs/index.html')
+
+def topics(request):
+  """Renders all topics in the learning log."""
+  topics = Topic.objects.order_by('date_added')
+  
+  # Define a context to send to the template
+  # A context is a dictionary which the keys are names we'll use in the template to access the data
+  context = {'topics': topics}
+  return render(request, 'learning_logs/topics.html', context)
