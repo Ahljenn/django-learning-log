3840c8b4019efc118119420853d79f9b804dafc4
Ahljenn <ahljennm2@gmail.com>
2022-08-05 00:35:57 -0700

    Added working topic routing for id

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,15 +5,18 @@ from . import views
 #Imports the views.py module from the same directory as the current urls.py module.
 
 app_name = "learning_logs"
+#Arg 1. URL / Path
+#Arg 2. Specifies which function to call in views.py.
+#Arg 3. Provides the name for this URL pattern to be referred to in other code sections.
 urlpatterns = [
-  #Arg 1. URL / Path
-  #Arg 2. Specifies which function to call in views.py.
-  #Arg 3. Provides the name for this URL pattern to be referred to in other code sections.
   
   # Home page
   path('', views.index, name='index'), #Home page
 
-  # Page for all topics
+  # Page for all topics.
   path('/topics', views.topics, name='topics'),
   
+  # Detail page for a single topic.
+  path('topics/<int:topic_id>/', views.topic, name='topic')
+
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -10,7 +10,17 @@ def topics(request):
   """Renders all topics in the learning log."""
   topics = Topic.objects.order_by('date_added')
   
-  # Define a context to send to the template
-  # A context is a dictionary which the keys are names we'll use in the template to access the data
+  # Define a context to send to the template.
+  # A context is a dictionary which the keys are names we'll use in the template to access the data.
   context = {'topics': topics}
   return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+  """Renders the single topic page and all its entries associated."""
+  topic = Topic.objects.get(id=topic_id)
+  entries = topic.entry_set.order_by('-date_added') # Minus sign sorts in reverse order.
+  context = {
+    'topic': topic,
+    'entries': entries
+  }
+  return render(request, 'learning_logs/topic.html', context)
