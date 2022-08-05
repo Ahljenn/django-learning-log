b9328e05c07b3607325db70e4538e153943edaef
Ahljenn <ahljennm2@gmail.com>
2022-08-04 19:49:30 -0700

    Create Topic Model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+  """A topic model that represents what the user is learning"""
+  text = models.CharField(max_length=200)
+  date_added = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    """Return a string representation of the topic model"""
+    return self.text
