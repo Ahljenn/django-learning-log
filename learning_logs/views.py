7b1a69dede2d329c7c660ee6ddf28ee7ea5c1ad5
Ahljenn <ahljennm2@gmail.com>
2022-08-05 23:19:56 -0700

    Working edit entry feature

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -45,7 +45,7 @@ def new_topic(request):
 
 
 def new_entry(request, topic_id):
-  """Returns the new entry page for a topic."""
+  """Renders the new entry page for a topic."""
   topic = Topic.objects.get(id=topic_id)
 
   if request.method != 'POST':
@@ -61,5 +61,31 @@ def new_entry(request, topic_id):
       return redirect('learning_logs:topic', topic_id=topic_id)
     
   # Display a blank or invalid form.
-  context = {'topic': topic, 'form': form}
+  context = {
+    'topic': topic, 
+    'form': form
+  }
   return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+  """Renders the page to edit an existing entry."""
+  entry = Entry.objects.get(id=entry_id)
+  topic = entry.topic
+
+  if request.method != 'POST':
+    # Initial request; pre-fill form with current entry. For a GET request.
+    form = EntryForm(instance=entry)
+  else:
+    # POST data submitted; process data.
+    form = EntryForm(instance=entry, data=request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('learning_logs:topic', topic_id=topic.id)
+  
+  context = {
+    'entry': entry,
+    'topic': topic,
+    'form': form
+  }
+
+  return render(request, 'learning_logs/edit_entry.html', context)
